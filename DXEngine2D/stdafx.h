ed3b61f985885ebbd0d6b54641d36128d6cf3582
GameDevTK <gamedevkannotatsuki@gmail.com>
2021-06-30 08:07:35 +0900

    【v0.00】windowsのエントリポイントを作成

---
+++ DXEngine2D/stdafx.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <memory>
+#include <math.h>
+#include <vector>
+#include <list>
+#include <D3DCompiler.h>
+#include <Windows.h>
+#include <d3d12.h>
+#include <map>
+#include <functional>
+#include <algorithm>
+#include <DirectXMath.h>
+#include <Xinput.h>
+#include <array>
+
+const UINT FRAME_BUFFER_W = 1280;	// フレームバッファの幅
+const UINT FRAME_BUFFER_H = 720;	// フレームバッファの高さ
